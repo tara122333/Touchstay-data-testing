964c4ffd58fcf0f4768b89d89afda377caa6b9d1
Tara Chand Kumawat <tarachand122333@gmail.com>
2026-04-15 18:38:59 +0530

    Simplify JSON response structure in credential route

--- routes/credential.routes.js
+++ routes/credential.routes.js
@@ -7,9 +7,7 @@ userRouter.get("/:id", async (req, res) => {
   const pageUrl = `https://guide.touchstay.com/guest/${id}/`;
   try {
     const responseData = await fetchTouchStayGuideResponse(pageUrl);
-    console.log("res", responseData);
-
-    return res.status(200).json({responseData});
+    return res.status(200).json(responseData);
   } catch (error) {
     res.status(500).send({
       success: false,
